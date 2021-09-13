9f48132b707b4b3654eb07c1959329cf294fbd0a
kuangkuangkuangha <75719597+kuangkuangkuangha@users.noreply.github.com>
2021-09-13 20:37:28 +0800

    update

---
+++ DataStructure/Unit1/1_5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+ // f(x)=a0+a1*x+a2*x*x 。。。
+int exci(int x, int n, int* a)
+{
+    int temp;
+    int sum =0;
+
+    for(int j=0; j<=n; j++)
+    {
+        temp=1;
+        temp=temp*a[j];
+        for(int k=0; k<j; k++)
+        {
+            temp=temp*x;
+        }
+        sum=sum+temp;
+    }
+
+    return sum;
+}
+
+
+
+
+int main()
+{
+    int x=2;
+    int n=3;
+    int a[4]={3,5,6,4};
+
+    cout<<exci(x,n,a)<<endl;
+}
